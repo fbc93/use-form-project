40af21fbd96f0c6219cee835c2c1197cf60922de
yhwa <hfbc93@gmail.com>
2023-09-10 20:27:13 +0900

    first commit

---
+++ app/components/swiper.tsx
@@ -0,0 +1,50 @@
+'client use'
+import { Swiper, SwiperSlide, SwiperProps } from 'swiper/react';
+import { Autoplay } from 'swiper';
+import 'swiper/css';
+import { useEffect, useLayoutEffect, useState } from 'react';
+
+export default function SwiperComponent() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const swiperOption: SwiperProps = {
+    slidesPerView: 3.5,
+    spaceBetween: 0,
+    loop: true,
+    modules: [Autoplay],
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1023: {
+        slidesPerView: 3.5,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      0: {
+        slidesPerView: 1,
+      },
+    }
+  };
+
+  useEffect(() => {
+    setLoading(true);
+  }, [])
+
+  return (
+    <>
+      {loading && (
+        <Swiper {...swiperOption}>
+          <SwiperSlide className='swiper-item'>Slide 1</SwiperSlide>
+          <SwiperSlide className='swiper-item'>Slide 2</SwiperSlide>
+          <SwiperSlide className='swiper-item'>Slide 3</SwiperSlide>
+          <SwiperSlide className='swiper-item'>Slide 4</SwiperSlide>
+          <SwiperSlide className='swiper-item'>Slide 5</SwiperSlide>
+          <SwiperSlide className='swiper-item'>Slide 6</SwiperSlide>
+          <SwiperSlide className='swiper-item'>Slide 7</SwiperSlide>
+        </Swiper>
+      )}
+    </>
+  );
+}
